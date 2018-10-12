1a660851f17db19391c756bc8e882ad9197cfa3b
Joao Victor Souza <joao.victor.souza99@hotmail.com>
2018-10-12 10:14:08 -0300

    Arrumando servidor

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// const express = require("express");
-// const app = express();
-// const bodyparser = require("body-parser");
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
 
-// app.get("/teste", (req, resp) => {
-//     resp.send("DEU CERTO!");
-// })
+var port =  process.env.PORT || processo.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip    =  process.env.IP || processo.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
-// app.listen("80");
-console.log("AAAAAAAAAAAAA");
+app.get("/teste", (req, resp) => {
+    resp.send("DEU CERTO ESSA PORRA!")
+});
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send('Alguma coisa ruim aconteceu!');
+});
+app.listen(port, ip);
+console.log('Servidor rodando em http://%s:%s', ip, port);
+module.exports = app;
