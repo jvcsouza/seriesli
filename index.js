c95f807c95e37c6082a987ea6d45e01de2e3edf4
Joao Victor Souza <joao.victor.souza99@hotmail.com>
2018-10-12 09:46:13 -0300

    comentando tudo, erro de container

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const app = express();
-const bodyparser = require("body-parser");
+// const express = require("express");
+// const app = express();
+// const bodyparser = require("body-parser");
 
-app.get("/teste", (req, resp) => {
-    resp.send("DEU CERTO!");
-})
+// app.get("/teste", (req, resp) => {
+//     resp.send("DEU CERTO!");
+// })
 
-app.listen("80");
+// app.listen("80");
+console.log("AAAAAAAAAAAAA");
